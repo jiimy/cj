b29b897822ad3ed15dfbcad3546733af4b26c002
rudwnok123 <rudwnok123@naver.com>
2020-08-19 21:29:34 +0900

    팝업 진행중

--- workspace/assets/script/popup.js
+++ workspace/assets/script/popup.js
@@ -13,13 +13,27 @@ function closepopup(closeBtn){
 }
 
 closepopup('.close-btn, .dim');
+closepopup('.btn-red');
 
 showpopup('.btn-complete-vote', '.complete-vote');
-showpopup('.event2 .card1 .detail', '.recipe1');
+// showpopup('.event2 .card1 .detail', '.recipe1');
 showpopup('.event2 .card2 .detail', '.recipe2');
 showpopup('.btn-correct-answer', '.correct-answer');
 showpopup('.btn-agree', '.agree');
 showpopup('.btn-false-answer', '.false-answer');
+
+$btn_idx: '';
+$('.event2 .card1 .detail').click(function(){
+  $('.recipe1').addClass('block');
+  $btn_idx = 1;
+})
+
+$('.btn-popup-next').click(function(){
+  if($btn_idx = 1) {
+    
+  }
+})
+
 
 // $('.detail').click(function(){
 //   $('.recipe').addClass('block');
