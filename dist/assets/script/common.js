6e45547f2c1271d085652fb7ad6fb759cdba3498
rudwnok123 <rudwnok123@naver.com>
2020-08-19 22:44:31 +0900

    개인정보 수집 토글

--- dist/assets/script/common.js
+++ dist/assets/script/common.js
@@ -1,3 +1,4 @@
 $('.btn-detail').click(function(){
+  $(this).closest('.agree').siblings('.agree').removeClass('is-on');
   $(this).closest('.agree').toggleClass('is-on');
-})
+});
--- dist/assets/script/popup.js
+++ dist/assets/script/popup.js
@@ -22,6 +22,11 @@ showpopup('.btn-correct-answer', '.correct-answer');
 showpopup('.btn-agree', '.agree');
 showpopup('.btn-false-answer', '.false-answer');
 
+
+// 레시피 팝업
+// $btn_idx = 0 일때 재료, 
+// $btn_idx = 1 일때 첫번째
+// $btn_idx = 2 일때 두번째
 $btn_idx: '';
 $('.event2 .card1 .detail').click(function(){
   $('.recipe1').addClass('block');
@@ -32,15 +37,12 @@ $('.event2 .card1 .detail').click(function(){
   $('.recipe1 .btn-popup-next').removeClass('none');
 })
 
-// $btn_idx = 0 일때 재료, 
-// $btn_idx = 1 일때 첫번째
-// $btn_idx = 2 일때 두번째
+
 $('.recipe1 .btn-popup-next').click(function(){
   switch ($btn_idx) {
     case 0:
       $('.recipe1 .list').addClass('none');
       $('.recipe1 .list1').removeClass('none');
-      console.log($btn_idx);
       $btn_idx = 1;
     break;
 
@@ -49,7 +51,6 @@ $('.recipe1 .btn-popup-next').click(function(){
       $('.recipe1 .list2').removeClass('none');
       $(this).addClass('none');
       $(this).closest('.popup__wrap').children('.btn-wrap ').removeClass('none');
-      // $btn_idx = 2;
     break;
     default:
       break;
